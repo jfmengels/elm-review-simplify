06791c6f2a98323a76b73a4968fd2c020bf6803d
Jeroen Engels <jfm.engels@gmail.com>
2020-03-05 17:48:58 +0100

    Add dummy config for elm-analyse (temporary)

---
+++ src/ReviewConfigForElmAnalyse.elm
@@ -0,0 +1,34 @@
+module ReviewConfigForElmAnalyse exposing (config)
+
+{-| Do not rename the ReviewConfig module or the config function, because
+`elm-review` will look for these.
+
+To add packages that contain rules, add them to this review project using
+
+    `elm install author/packagename`
+
+when inside the directory containing this file.
+
+-}
+
+-- import NoDebug
+-- import NoUnused.CustomTypeConstructors
+-- import NoUnused.Variables
+
+import NoAddingListLiterals
+import NoBooleanCaseOf
+import NoPrefixOperator
+import Review.Rule exposing (Rule)
+
+
+config : List Rule
+config =
+    [ NoAddingListLiterals.rule
+    , NoBooleanCaseOf.rule
+
+    -- , NoDebug.rule
+    , NoPrefixOperator.rule
+
+    -- , NoUnused.CustomTypeConstructors.rule
+    -- , NoUnused.Variables.rule
+    ]
