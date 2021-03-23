cd0feac417e68cc8ca29144d4d3007bbb20a8634
Jeroen Engels <jfm.engels@gmail.com>
2021-03-24 00:01:24 +0100

    Add failing test

--- tests/Simplify/IfsTest.elm
+++ tests/Simplify/IfsTest.elm
@@ -1,24 +1,43 @@
 module Simplify.IfsTest exposing (all)
 
-import Simplify.Ifs exposing (rule)
 import Review.Test
+import Simplify.Ifs exposing (rule)
 import Test exposing (Test, describe, test)
 
 
 all : Test
 all =
     describe "Simplify.Ifs"
-        [ test "should report an error when REPLACEME" <|
+        [ test "should remove the else branch when a condition is True" <|
             \() ->
                 """module A exposing (..)
-a = 1
+a = if True then 1 else 2
 """
                     |> Review.Test.run rule
                     |> Review.Test.expectErrors
                         [ Review.Test.error
                             { message = "REPLACEME"
                             , details = [ "REPLACEME" ]
-                            , under = "REPLACEME"
+                            , under = "if"
                             }
+                            |> Review.Test.whenFixed """module A exposing (..)
+a = 1
+"""
+                        ]
+        , test "should remove the if branch when a condition is False" <|
+            \() ->
+                """module A exposing (..)
+a = if False then 1 else 2
+"""
+                    |> Review.Test.run rule
+                    |> Review.Test.expectErrors
+                        [ Review.Test.error
+                            { message = "REPLACEME"
+                            , details = [ "REPLACEME" ]
+                            , under = "if"
+                            }
+                            |> Review.Test.whenFixed """module A exposing (..)
+a = 2
+"""
                         ]
         ]
