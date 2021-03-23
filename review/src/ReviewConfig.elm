4e14887f124e915f16b00e27ef84f6a135edee90
Jeroen Engels <jfm.engels@gmail.com>
2021-03-23 23:49:05 +0100

    Update elm-review deps and rules

--- review/src/ReviewConfig.elm
+++ review/src/ReviewConfig.elm
@@ -11,35 +11,50 @@ when inside the directory containing this file.
 
 -}
 
+import Documentation.ReadmeLinksPointToCurrentVersion
 import NoBooleanCaseOf
 import NoDebug.Log
 import NoDebug.TodoOrToString
 import NoExposingEverything
+import NoForbiddenWords
 import NoFullyAppliedPrefixOperator
 import NoImportingEverything
 import NoListLiteralsConcat
 import NoMissingTypeAnnotation
+import NoMissingTypeAnnotationInLetIn
+import NoMissingTypeExpose
+import NoUnused.CustomTypeConstructorArgs
 import NoUnused.CustomTypeConstructors
 import NoUnused.Dependencies
 import NoUnused.Exports
 import NoUnused.Modules
+import NoUnused.Parameters
+import NoUnused.Patterns
 import NoUnused.Variables
-import Review.Rule exposing (Rule)
+import Review.Rule as Rule exposing (Rule)
 
 
 config : List Rule
 config =
     [ NoBooleanCaseOf.rule
+    , Documentation.ReadmeLinksPointToCurrentVersion.rule
     , NoDebug.Log.rule
     , NoDebug.TodoOrToString.rule
+        |> Rule.ignoreErrorsForDirectories [ "tests/" ]
     , NoExposingEverything.rule
     , NoFullyAppliedPrefixOperator.rule
+    , NoForbiddenWords.rule [ "REPLACEME" ]
     , NoImportingEverything.rule []
     , NoListLiteralsConcat.rule
     , NoMissingTypeAnnotation.rule
+    , NoMissingTypeAnnotationInLetIn.rule
+    , NoMissingTypeExpose.rule
     , NoUnused.CustomTypeConstructors.rule []
+    , NoUnused.CustomTypeConstructorArgs.rule
     , NoUnused.Dependencies.rule
     , NoUnused.Exports.rule
     , NoUnused.Modules.rule
+    , NoUnused.Parameters.rule
+    , NoUnused.Patterns.rule
     , NoUnused.Variables.rule
     ]
