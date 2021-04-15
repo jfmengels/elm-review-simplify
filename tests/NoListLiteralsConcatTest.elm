5549b688e8828e08fe7ff55c03126f270bc9e11f
Jeroen Engels <jfm.engels@gmail.com>
2021-04-15 23:08:38 +0200

    Fix List.filter functions with 1 arg using pipelines

--- tests/NoListLiteralsConcatTest.elm
+++ tests/NoListLiteralsConcatTest.elm
@@ -499,6 +499,38 @@ a = List.filter (always True)
 a = identity
 """
                         ]
+        , test "should replace List.filter <| (always True) by identity" <|
+            \() ->
+                """module A exposing (..)
+a = List.filter <| (always True)
+"""
+                    |> Review.Test.run rule
+                    |> Review.Test.expectErrors
+                        [ Review.Test.error
+                            { message = "Using List.filter with a function that will always return True is the same as not using List.filter"
+                            , details = [ "You can remove this call and replace it by the list itself" ]
+                            , under = "List.filter"
+                            }
+                            |> Review.Test.whenFixed """module A exposing (..)
+a = identity
+"""
+                        ]
+        , test "should replace always True |> List.filter by identity" <|
+            \() ->
+                """module A exposing (..)
+a = always True |> List.filter
+"""
+                    |> Review.Test.run rule
+                    |> Review.Test.expectErrors
+                        [ Review.Test.error
+                            { message = "Using List.filter with a function that will always return True is the same as not using List.filter"
+                            , details = [ "You can remove this call and replace it by the list itself" ]
+                            , under = "List.filter"
+                            }
+                            |> Review.Test.whenFixed """module A exposing (..)
+a = identity
+"""
+                        ]
         , test "should replace List.filter (always False) x by []" <|
             \() ->
                 """module A exposing (..)
@@ -519,6 +551,38 @@ a = []
             \() ->
                 """module A exposing (..)
 a = List.filter (always False)
+"""
+                    |> Review.Test.run rule
+                    |> Review.Test.expectErrors
+                        [ Review.Test.error
+                            { message = "Using List.filter with a function that will always return False will result in an empty list"
+                            , details = [ "You can remove this call and replace it by an empty list" ]
+                            , under = "List.filter"
+                            }
+                            |> Review.Test.whenFixed """module A exposing (..)
+a = (always [])
+"""
+                        ]
+        , test "should replace List.filter <| (always False) by always []" <|
+            \() ->
+                """module A exposing (..)
+a = List.filter <| (always False)
+"""
+                    |> Review.Test.run rule
+                    |> Review.Test.expectErrors
+                        [ Review.Test.error
+                            { message = "Using List.filter with a function that will always return False will result in an empty list"
+                            , details = [ "You can remove this call and replace it by an empty list" ]
+                            , under = "List.filter"
+                            }
+                            |> Review.Test.whenFixed """module A exposing (..)
+a = (always [])
+"""
+                        ]
+        , test "should replace always False |> List.filter by always []" <|
+            \() ->
+                """module A exposing (..)
+a = always False |> List.filter
 """
                     |> Review.Test.run rule
                     |> Review.Test.expectErrors
