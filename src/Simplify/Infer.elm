a526eda8d0f6627a0ab797aecdaef14713baa89e
Jeroen Engels <jfm.engels@gmail.com>
2022-08-12 23:38:51 +0200

    Remove unused code

--- src/Simplify/Infer.elm
+++ src/Simplify/Infer.elm
@@ -1,16 +1,13 @@
 module Simplify.Infer exposing
-    ( Constraint(..)
-    , Constraint2(..)
+    ( Constraint2(..)
     , DeducedValue(..)
     , Inferred2(..)
     , Resources
-    , empty
     , empty2
     , falseExpr
     , get2
     , getInt
     , infer2
-    , inferForIfCondition
     , inferForIfCondition2
     , isBoolean
     , mergeConstraints
@@ -23,10 +20,6 @@ import Elm.Syntax.Node as Node exposing (Node(..))
 import Elm.Syntax.Range exposing (Range)
 import Review.ModuleNameLookupTable as ModuleNameLookupTable exposing (ModuleNameLookupTable)
 import Simplify.AstHelpers as AstHelpers
-
-
-type Inferred
-    = Inferred (AssocList.Dict Expression Constraints)
 
 
 type Inferred2
@@ -49,26 +42,11 @@ type Constraint2
     | Or2 Constraint2 Constraint2
 
 
-type Constraints
-    = Single Constraint
-
-
-type Constraint
-    = Equals Expression
-    | NotEquals Expression
-    | Is Bool
-
-
 type alias Resources a =
     { a
         | lookupTable : ModuleNameLookupTable
         , inferredConstants2 : ( Inferred2, List Inferred2 )
     }
-
-
-empty : Inferred
-empty =
-    Inferred AssocList.empty
 
 
 empty2 : Inferred2
@@ -453,145 +431,6 @@ inferOnEquality2 (Node _ expr) (Node _ other) shouldBe dict =
 
         _ ->
             dict
-
-
-inferForIfCondition : Expression -> { trueBranchRange : Range, falseBranchRange : Range } -> Inferred -> List ( Range, Inferred )
-inferForIfCondition condition { trueBranchRange, falseBranchRange } inferred =
-    [ ( trueBranchRange, infer [ condition ] (Single (Is True)) inferred )
-    , ( falseBranchRange, infer [ condition ] (Single (Is False)) inferred )
-    ]
-
-
-infer : List Expression -> Constraints -> Inferred -> Inferred
-infer nodes constraints acc =
-    case nodes of
-        [] ->
-            acc
-
-        node :: rest ->
-            let
-                dict : Inferred
-                dict =
-                    injectConstraints node constraints acc
-            in
-            case node of
-                Expression.FunctionOrValue _ _ ->
-                    infer rest constraints dict
-
-                Expression.Application [ Node _ (Expression.FunctionOrValue [ "Basics" ] "not"), expression ] ->
-                    infer
-                        rest
-                        constraints
-                        (infer [ Node.value expression ] (inverseConstraint constraints) dict)
-
-                Expression.OperatorApplication "&&" _ left right ->
-                    if constraints == Single (Is True) then
-                        infer (Node.value left :: Node.value right :: rest) constraints dict
-
-                    else
-                        -- TODO Add inverse constraint
-                        infer rest constraints dict
-
-                Expression.OperatorApplication "||" _ left right ->
-                    if constraints == Single (Is False) then
-                        infer (Node.value left :: Node.value right :: rest) constraints dict
-
-                    else
-                        -- TODO Add inverse constraint
-                        infer rest constraints dict
-
-                Expression.OperatorApplication "==" _ left right ->
-                    infer rest
-                        constraints
-                        (dict
-                            |> inferOnEquality left right constraints
-                            |> inferOnEquality right left constraints
-                        )
-
-                Expression.OperatorApplication "/=" _ left right ->
-                    let
-                        inversedConstraints : Constraints
-                        inversedConstraints =
-                            inverseConstraint constraints
-                    in
-                    infer rest
-                        constraints
-                        (dict
-                            |> inferOnEquality left right inversedConstraints
-                            |> inferOnEquality right left inversedConstraints
-                        )
-
-                _ ->
-                    infer rest constraints dict
-
-
-inferOnEquality : Node Expression -> Node Expression -> Constraints -> Inferred -> Inferred
-inferOnEquality (Node _ expr) (Node _ other) constraints dict =
-    case expr of
-        Expression.Integer int ->
-            case constraints of
-                Single (Is True) ->
-                    injectConstraints
-                        other
-                        (Single (Equals (Expression.Floatable (Basics.toFloat int))))
-                        dict
-
-                Single (Is False) ->
-                    injectConstraints
-                        other
-                        (Single (NotEquals (Expression.Floatable (Basics.toFloat int))))
-                        dict
-
-                Single _ ->
-                    dict
-
-        _ ->
-            dict
-
-
-inverseConstraint : Constraints -> Constraints
-inverseConstraint (Single constraint) =
-    Single (inverseSingleConstraint constraint)
-
-
-inverseSingleConstraint : Constraint -> Constraint
-inverseSingleConstraint constraint =
-    case constraint of
-        Is bool ->
-            Is (not bool)
-
-        Equals (Expression.FunctionOrValue [ "Basics" ] "True") ->
-            Is False
-
-        Equals (Expression.FunctionOrValue [ "Basics" ] "False") ->
-            Is True
-
-        Equals value ->
-            NotEquals value
-
-        NotEquals value ->
-            Equals value
-
-
-injectConstraints : Expression -> Constraints -> Inferred -> Inferred
-injectConstraints expression constraints (Inferred inferred) =
-    inferred
-        |> AssocList.foldl
-            (\expr v acc ->
-                case expr of
-                    Expression.OperatorApplication "&&" _ (Node _ left) (Node _ right) ->
-                        if expression == left then
-                            AssocList.insert expr v acc
-
-                        else
-                            AssocList.insert expr v acc
-
-                    _ ->
-                        AssocList.insert expr v acc
-            )
-            AssocList.empty
-        |> AssocList.insert expression constraints
-        |> Inferred
 
 
 getInt : Resources a -> Node Expression -> Maybe Int
