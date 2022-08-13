7b9d6bd96250a81743362434e3094b33670eb932
Jeroen Engels <jfm.engels@gmail.com>
2022-08-13 18:24:19 +0200

    Add tests about inequality

--- tests/SimplifyTest.elm
+++ tests/SimplifyTest.elm
@@ -3443,6 +3443,37 @@ a =
     3
 """
                         ]
+        , test "should remove branches where the condition never matches (strings)" <|
+            \() ->
+                """module A exposing (..)
+a =
+  if x /= "a" then
+    if x == "a" then
+      1
+    else
+      2
+  else
+    3
+"""
+                    |> Review.Test.run (rule defaults)
+                    |> Review.Test.expectErrors
+                        [ Review.Test.error
+                            { message = "Comparison is always False"
+                            , details = sameThingOnBothSidesDetails "False"
+                            , under = "x == \"a\""
+                            }
+                            |> Review.Test.atExactly { start = { row = 4, column = 8 }, end = { row = 4, column = 16 } }
+                            |> Review.Test.whenFixed """module A exposing (..)
+a =
+  if x /= "a" then
+    if False then
+      1
+    else
+      2
+  else
+    3
+"""
+                        ]
         , test "should not spread inferred things from one branch to another" <|
             \() ->
                 """module A exposing (..)
@@ -3529,6 +3560,35 @@ a =
                         [ Review.Test.error
                             { message = "Comparison is always True"
                             , details = sameThingOnBothSidesDetails "True"
+                            , under = "x == 1"
+                            }
+                            |> Review.Test.whenFixed """module A exposing (..)
+a =
+  if x /= 1 then
+    1
+  else if True then
+    2
+  else
+    3
+"""
+                        ]
+        , test "should remove branches where the condition always matches (/= <then> == in else)" <|
+            \() ->
+                """module A exposing (..)
+a =
+  if x /= 1 then
+    if x == 1 then
+      1
+    else
+      2
+  else
+    3
+"""
+                    |> Review.Test.run (rule defaults)
+                    |> Review.Test.expectErrors
+                        [ Review.Test.error
+                            { message = "Comparison is always False"
+                            , details = sameThingOnBothSidesDetails "False"
                             , under = "x == 1"
                             }
                             |> Review.Test.whenFixed """module A exposing (..)
