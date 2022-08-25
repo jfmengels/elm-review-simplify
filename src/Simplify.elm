e3941e90d2d1ddcbda3da523ef45209a8faa1eaa
Jeroen Engels <jfm.engels@gmail.com>
2022-08-25 21:59:05 +0200

    Remove withContextFromImportedModules

--- src/Simplify.elm
+++ src/Simplify.elm
@@ -616,7 +616,6 @@ rule (Configuration config) =
                     , fromModuleToProject = fromModuleToProject
                     , foldProjectContexts = foldProjectContexts
                     }
-                |> Rule.withContextFromImportedModules
                 |> Rule.withFinalProjectEvaluation (finalEvaluation config.ignoreConstructors)
                 |> Rule.fromProjectRuleSchema
 
