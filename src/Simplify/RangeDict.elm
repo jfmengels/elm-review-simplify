868613e257126acad83d5654bbaaf156c5b7086d
lue-bird <lue.the.bird@gmail.com>
2023-03-11 13:45:03 +0100

    Make `RangeDict` opaque

--- src/Simplify/RangeDict.elm
+++ src/Simplify/RangeDict.elm
@@ -4,60 +4,61 @@ import Dict exposing (Dict)
 import Elm.Syntax.Range exposing (Range)
 
 
-type alias RangeDict v =
-    -- TODO: make opaque to disallow type confusion with other String dicts
-    Dict String v
+type RangeDict v
+    = RangeDict (Dict String v)
 
 
 empty : RangeDict v
 empty =
-    Dict.empty
+    RangeDict Dict.empty
 
 
 singleton : Range -> v -> RangeDict v
 singleton range value =
-    Dict.singleton (rangeAsString range) value
+    RangeDict (Dict.singleton (rangeAsString range) value)
 
 
 {-| Indirect conversion from a list to key-value pairs to avoid successive List.map calls.
 -}
 mapFromList : (a -> ( Range, v )) -> List a -> RangeDict v
 mapFromList toAssociation list =
-    Dict.fromList
-        (List.map
-            (\element ->
-                let
-                    ( range, v ) =
-                        toAssociation element
-                in
-                ( rangeAsString range, v )
+    RangeDict
+        (Dict.fromList
+            (List.map
+                (\element ->
+                    let
+                        ( range, v ) =
+                            toAssociation element
+                    in
+                    ( rangeAsString range, v )
+                )
+                list
             )
-            list
         )
 
 
 insert : Range -> v -> RangeDict v -> RangeDict v
-insert range value rangeDict =
-    Dict.insert (rangeAsString range) value rangeDict
+insert range value (RangeDict rangeDict) =
+    RangeDict (Dict.insert (rangeAsString range) value rangeDict)
 
 
 remove : Range -> RangeDict v -> RangeDict v
-remove range rangeDict =
-    Dict.remove (rangeAsString range) rangeDict
+remove range (RangeDict rangeDict) =
+    RangeDict (Dict.remove (rangeAsString range) rangeDict)
 
 
 get : Range -> RangeDict v -> Maybe v
-get range rangeDict =
+get range (RangeDict rangeDict) =
     Dict.get (rangeAsString range) rangeDict
 
 
 member : Range -> RangeDict v -> Bool
-member range rangeDict =
+member range (RangeDict rangeDict) =
     Dict.member (rangeAsString range) rangeDict
 
 
 foldl : (v -> folded -> folded) -> folded -> RangeDict v -> folded
-foldl reduce initialFolded rangeDict =
+foldl reduce initialFolded (RangeDict rangeDict) =
     Dict.foldl (\_ -> reduce) initialFolded rangeDict
 
 
@@ -69,8 +70,8 @@ any isFound rangeDict =
 
 
 union : RangeDict v -> RangeDict v -> RangeDict v
-union aRangeDict bRangeDict =
-    Dict.union aRangeDict bRangeDict
+union (RangeDict aRangeDict) (RangeDict bRangeDict) =
+    RangeDict (Dict.union aRangeDict bRangeDict)
 
 
 rangeAsString : Range -> String
