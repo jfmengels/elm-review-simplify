0161c0ec2507934b5cbb4e3de7ae3ab012529ec4
lue-bird <lue.the.bird@gmail.com>
2023-03-13 17:02:46 +0100

    Add lambda reduce tests

--- tests/SimplifyTest.elm
+++ tests/SimplifyTest.elm
@@ -11,6 +11,7 @@ all =
     describe "Simplify"
         [ configurationTests
         , qualifyTests
+        , lambdaReduceTests
         , identityTests
         , alwaysTests
         , booleanTests
@@ -667,6 +668,132 @@ a =
         [ \\foldl -> doIt
         , (\\foldl -> doIt) >> (\\x -> foldl f x)
         ]
+"""
+                        ]
+        ]
+
+
+
+-- LAMBDA REDUCE
+
+
+lambdaReduceTests : Test
+lambdaReduceTests =
+    describe "lambda reduce"
+        [ test "should detect (\\error -> Err error) as Err function" <|
+            \() ->
+                """module A exposing (..)
+a = Result.mapError f << (\\error -> Err error)
+"""
+                    |> Review.Test.run ruleWithDefaults
+                    |> Review.Test.expectErrors
+                        [ Review.Test.error
+                            { message = "Using Result.mapError on Err will result in Err with the function applied to the error"
+                            , details = [ "You can replace this call by Err with the function directly applied to the error itself." ]
+                            , under = "Result.mapError"
+                            }
+                            |> Review.Test.whenFixed """module A exposing (..)
+a = Err << f
+"""
+                        ]
+        , test "should detect (\\error -> error |> Err) as Err function" <|
+            \() ->
+                -- Err in practice only takes 1 argument; this is just for testing reducing functionality
+                """module A exposing (..)
+a = Result.mapError f << (\\error -> error |> Err)
+"""
+                    |> Review.Test.run ruleWithDefaults
+                    |> Review.Test.expectErrors
+                        [ Review.Test.error
+                            { message = "Using Result.mapError on Err will result in Err with the function applied to the error"
+                            , details = [ "You can replace this call by Err with the function directly applied to the error itself." ]
+                            , under = "Result.mapError"
+                            }
+                            |> Review.Test.whenFixed """module A exposing (..)
+a = Err << f
+"""
+                        ]
+        , test "should detect (\\error -> Err <| error) as Err function" <|
+            \() ->
+                -- Err in practice only takes 1 argument; this is just for testing reducing functionality
+                """module A exposing (..)
+a = Result.mapError f << (\\error -> Err <| error)
+"""
+                    |> Review.Test.run ruleWithDefaults
+                    |> Review.Test.expectErrors
+                        [ Review.Test.error
+                            { message = "Using Result.mapError on Err will result in Err with the function applied to the error"
+                            , details = [ "You can replace this call by Err with the function directly applied to the error itself." ]
+                            , under = "Result.mapError"
+                            }
+                            |> Review.Test.whenFixed """module A exposing (..)
+a = Err << f
+"""
+                        ]
+        , test "should detect (\\error sorry -> (error |> Err) <| sorry) as Err function" <|
+            \() ->
+                -- Err in practice only takes 1 argument; this is just for testing reducing functionality
+                """module A exposing (..)
+a = Result.mapError f << (\\error -> \\sorry -> (error |> Err) <| sorry)
+"""
+                    |> Review.Test.run ruleWithDefaults
+                    |> Review.Test.expectErrors
+                        [ Review.Test.error
+                            { message = "Using Result.mapError on Err will result in Err with the function applied to the error"
+                            , details = [ "You can replace this call by Err with the function directly applied to the error itself." ]
+                            , under = "Result.mapError"
+                            }
+                            |> Review.Test.whenFixed """module A exposing (..)
+a = Err << f
+"""
+                        ]
+        , test "should detect (\\error -> \\sorry -> (error |> Err) <| sorry) as Err function" <|
+            \() ->
+                -- Err in practice only takes 1 argument; this is just for testing reducing functionality
+                """module A exposing (..)
+a = Result.mapError f << (\\error -> \\sorry -> (error |> Err) <| sorry)
+"""
+                    |> Review.Test.run ruleWithDefaults
+                    |> Review.Test.expectErrors
+                        [ Review.Test.error
+                            { message = "Using Result.mapError on Err will result in Err with the function applied to the error"
+                            , details = [ "You can replace this call by Err with the function directly applied to the error itself." ]
+                            , under = "Result.mapError"
+                            }
+                            |> Review.Test.whenFixed """module A exposing (..)
+a = Err << f
+"""
+                        ]
+        , test "should detect (\\result -> Result.mapError f result) as Result.mapError f call" <|
+            \() ->
+                """module A exposing (..)
+a = (\\result -> Result.mapError f result) << Err
+"""
+                    |> Review.Test.run ruleWithDefaults
+                    |> Review.Test.expectErrors
+                        [ Review.Test.error
+                            { message = "Using Result.mapError on Err will result in Err with the function applied to the error"
+                            , details = [ "You can replace this call by Err with the function directly applied to the error itself." ]
+                            , under = "Result.mapError"
+                            }
+                            |> Review.Test.whenFixed """module A exposing (..)
+a = Err << f
+"""
+                        ]
+        , test "should detect (\\result -> \\sorry -> (result |> Result.mapError f) <| sorry) as Result.mapError f call" <|
+            \() ->
+                """module A exposing (..)
+a = (\\result -> \\sorry -> (result |> Result.mapError f) <| sorry) << Err
+"""
+                    |> Review.Test.run ruleWithDefaults
+                    |> Review.Test.expectErrors
+                        [ Review.Test.error
+                            { message = "Using Result.mapError on Err will result in Err with the function applied to the error"
+                            , details = [ "You can replace this call by Err with the function directly applied to the error itself." ]
+                            , under = "Result.mapError"
+                            }
+                            |> Review.Test.whenFixed """module A exposing (..)
+a = Err << f
 """
                         ]
         ]
