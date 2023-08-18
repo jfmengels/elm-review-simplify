c35fccf5ad1a83882c13b0fa2929ab7fb5ef8a4f
lue-bird <lue.the.bird@gmail.com>
2023-08-18 13:45:18 +0200

    Remove unused export getSpecificReducedFunction

--- src/Simplify/AstHelpers.elm
+++ src/Simplify/AstHelpers.elm
@@ -14,7 +14,6 @@ module Simplify.AstHelpers exposing
     , getNotFunction
     , getOrder
     , getSpecificFunctionCall
-    , getSpecificReducedFunction
     , getSpecificReducedFunctionCall
     , getSpecificValueOrFunction
     , getTuple
