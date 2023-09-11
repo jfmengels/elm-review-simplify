a61d9d911c38229429c932767d0ad79e168e4182
lue-bird <lue.the.bird@gmail.com>
2023-09-11 19:46:37 +0200

    WrapperProperties documentation correct

--- src/Simplify.elm
+++ src/Simplify.elm
@@ -6119,7 +6119,8 @@ type alias ConstantProperties =
 
 {-| Properties of a type that has a construction function that takes one value.
 
-Example "wrap" construction functions: `Just`, `Err`, `List.singleton` and `[ a ]`, `Cmd.batch [ a ]`
+Example "wrap" construction functions: `Just`, `Err`, `List.singleton` and `[ a ]`
+Note that for example `Cmd.batch [ a ]` is not a "warp" because it keeps the type of the inner value `a`
 
 -}
 type alias WrapperProperties otherProperties =
