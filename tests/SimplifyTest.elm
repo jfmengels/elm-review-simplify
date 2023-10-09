7b25e9c30c8e8697479a98ce53c35f08fc7390ec
Jeroen Engels <jfm.engels@gmail.com>
2023-10-09 21:41:21 +0200

    Split up tests for configuration errors

--- tests/SimplifyTest.elm
+++ tests/SimplifyTest.elm
@@ -1,149 +1,20 @@
 module SimplifyTest exposing (all)
 
 import Review.Test
-import Simplify exposing (defaults, ignoreCaseOfForTypes, rule)
 import Test exposing (Test, describe, test)
-import TestHelpers exposing (ruleExpectingNaN, ruleWithDefaults)
+import TestHelpers exposing (ruleWithDefaults)
 
 
 all : Test
 all =
     describe "Simplify"
-        [ configurationTests
-        , identityTests
+        [ identityTests
         , alwaysTests
         , toFloatTests
         , roundTests
         , ceilingTests
         , floorTests
         , truncateTests
-        ]
-
-
-
--- CONFIGURATION
-
-
-configurationTests : Test
-configurationTests =
-    describe "Configuration"
-        [ test "should not report configuration error if all ignored constructors exist" <|
-            \() ->
-                """module A exposing (..)
-type B = B
-type C = C
-"""
-                    |> Review.Test.run (rule <| ignoreCaseOfForTypes [ "Maybe.Maybe", "Result.Result" ] defaults)
-                    |> Review.Test.expectNoErrors
-        , test "should report configuration error if passed an invalid module name" <|
-            \() ->
-                """module A exposing (..)
-a = 1
-"""
-                    |> Review.Test.run (rule <| ignoreCaseOfForTypes [ "_.B" ] defaults)
-                    |> Review.Test.expectGlobalErrors
-                        [ { message = "Could not find type names: `_.B`"
-                          , details =
-                                [ "I expected to find these custom types in the dependencies, but I could not find them."
-                                , "Please check whether these types and have not been removed, and if so, remove them from the configuration of this rule."
-                                , "If you find that these types have been moved or renamed, please update your configuration."
-                                , "Note that I may have provided fixes for things you didn't wish to be fixed, so you might want to undo the changes I have applied."
-                                , "Also note that the configuration for this rule changed in v2.0.19: types that are custom to your project are ignored by default, so this configuration setting can only be used to avoid simplifying case expressions that use custom types defined in dependencies."
-                                ]
-                          }
-                        ]
-        , test "should report configuration error if passed an invalid type name" <|
-            \() ->
-                """module A exposing (..)
-a = 1
-"""
-                    |> Review.Test.run (rule <| ignoreCaseOfForTypes [ "A.f" ] defaults)
-                    |> Review.Test.expectGlobalErrors
-                        [ { message = "Could not find type names: `A.f`"
-                          , details =
-                                [ "I expected to find these custom types in the dependencies, but I could not find them."
-                                , "Please check whether these types and have not been removed, and if so, remove them from the configuration of this rule."
-                                , "If you find that these types have been moved or renamed, please update your configuration."
-                                , "Note that I may have provided fixes for things you didn't wish to be fixed, so you might want to undo the changes I have applied."
-                                , "Also note that the configuration for this rule changed in v2.0.19: types that are custom to your project are ignored by default, so this configuration setting can only be used to avoid simplifying case expressions that use custom types defined in dependencies."
-                                ]
-                          }
-                        ]
-        , test "should report configuration error if passed an empty type name" <|
-            \() ->
-                """module A exposing (..)
-a = 1
-"""
-                    |> Review.Test.run (rule <| ignoreCaseOfForTypes [ "" ] defaults)
-                    |> Review.Test.expectGlobalErrors
-                        [ { message = "Could not find type names: ``"
-                          , details =
-                                [ "I expected to find these custom types in the dependencies, but I could not find them."
-                                , "Please check whether these types and have not been removed, and if so, remove them from the configuration of this rule."
-                                , "If you find that these types have been moved or renamed, please update your configuration."
-                                , "Note that I may have provided fixes for things you didn't wish to be fixed, so you might want to undo the changes I have applied."
-                                , "Also note that the configuration for this rule changed in v2.0.19: types that are custom to your project are ignored by default, so this configuration setting can only be used to avoid simplifying case expressions that use custom types defined in dependencies."
-                                ]
-                          }
-                        ]
-        , test "should report configuration error if passed a type name without a module name" <|
-            \() ->
-                """module A exposing (..)
-a = 1
-"""
-                    |> Review.Test.run (rule <| ignoreCaseOfForTypes [ "B" ] defaults)
-                    |> Review.Test.expectGlobalErrors
-                        [ { message = "Could not find type names: `B`"
-                          , details =
-                                [ "I expected to find these custom types in the dependencies, but I could not find them."
-                                , "Please check whether these types and have not been removed, and if so, remove them from the configuration of this rule."
-                                , "If you find that these types have been moved or renamed, please update your configuration."
-                                , "Note that I may have provided fixes for things you didn't wish to be fixed, so you might want to undo the changes I have applied."
-                                , "Also note that the configuration for this rule changed in v2.0.19: types that are custom to your project are ignored by default, so this configuration setting can only be used to avoid simplifying case expressions that use custom types defined in dependencies."
-                                ]
-                          }
-                        ]
-        , test "should report configuration error if passed multiple invalid types" <|
-            \() ->
-                """module A exposing (..)
-a = 1
-"""
-                    |> Review.Test.run (rule <| ignoreCaseOfForTypes [ "_.B", "A.f", "B", "Maybe.Maybe" ] defaults)
-                    |> Review.Test.expectGlobalErrors
-                        [ { message = "Could not find type names: `A.f`, `B`, `_.B`"
-                          , details =
-                                [ "I expected to find these custom types in the dependencies, but I could not find them."
-                                , "Please check whether these types and have not been removed, and if so, remove them from the configuration of this rule."
-                                , "If you find that these types have been moved or renamed, please update your configuration."
-                                , "Note that I may have provided fixes for things you didn't wish to be fixed, so you might want to undo the changes I have applied."
-                                , "Also note that the configuration for this rule changed in v2.0.19: types that are custom to your project are ignored by default, so this configuration setting can only be used to avoid simplifying case expressions that use custom types defined in dependencies."
-                                ]
-                          }
-                        ]
-        , test "should report global error if ignored types were not found in the project" <|
-            \() ->
-                """module A exposing (..)
-a = 1
-"""
-                    |> Review.Test.run (rule <| ignoreCaseOfForTypes [ "A.B", "B.C" ] defaults)
-                    |> Review.Test.expectGlobalErrors
-                        [ { message = "Could not find type names: `A.B`, `B.C`"
-                          , details =
-                                [ "I expected to find these custom types in the dependencies, but I could not find them."
-                                , "Please check whether these types and have not been removed, and if so, remove them from the configuration of this rule."
-                                , "If you find that these types have been moved or renamed, please update your configuration."
-                                , "Note that I may have provided fixes for things you didn't wish to be fixed, so you might want to undo the changes I have applied."
-                                , "Also note that the configuration for this rule changed in v2.0.19: types that are custom to your project are ignored by default, so this configuration setting can only be used to avoid simplifying case expressions that use custom types defined in dependencies."
-                                ]
-                          }
-                        ]
-        , test "should not report global error if ignored type was found in the dependencies" <|
-            \() ->
-                """module A exposing (..)
-a = 1
-"""
-                    |> Review.Test.run (rule <| ignoreCaseOfForTypes [ "Maybe.Maybe" ] defaults)
-                    |> Review.Test.expectNoErrors
         ]
 
 
