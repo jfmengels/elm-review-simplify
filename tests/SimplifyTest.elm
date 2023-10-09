857de9328340489c33e33dca89f4350c58644595
Jeroen Engels <jfm.engels@gmail.com>
2023-10-09 20:38:15 +0200

    Split up tests for let declarations

--- tests/SimplifyTest.elm
+++ tests/SimplifyTest.elm
@@ -45,7 +45,6 @@ all =
         , htmlAttributesTests
         , randomTests
         , recordAccessTests
-        , letTests
         ]
 
 
@@ -29739,89 +29738,4 @@ a = (if x then { f = 3 }.f else if y then {f = 2}.f else
                         ]
         ]
 
-
-letTests : Test
-letTests =
-    describe "Let declarations"
-        [ test "should merge two adjacent let declarations" <|
-            \() ->
-                """module A exposing (..)
-a =
-    let
-        b =
-            1
-    in
-    let
-        c =
-            1
-    in
-    b + c
-"""
-                    |> Review.Test.run ruleWithDefaults
-                    |> Review.Test.expectErrors
-                        [ Review.Test.error
-                            { message = "Let blocks can be joined together"
-                            , details = [ "Let blocks can contain multiple declarations, and there is no advantage to having multiple chained let expressions rather than one longer let expression." ]
-                            , under = "let"
-                            }
-                            |> Review.Test.atExactly { start = { row = 7, column = 5 }, end = { row = 7, column = 8 } }
-                            |> Review.Test.whenFixed """module A exposing (..)
-a =
-    let
-        b =
-            1
-
-        c =
-            1
-    in
-    b + c
-"""
-                        ]
-        , test "should merge two adjacent let declarations with multiple declarations" <|
-            \() ->
-                """module A exposing (..)
-a =
-    let
-        b =
-            1
-
-        c =
-            1
-    in
-    let
-        d =
-            1
-
-        e =
-            1
-    in
-    b + c + d + e
-"""
-                    |> Review.Test.run ruleWithDefaults
-                    |> Review.Test.expectErrors
-                        [ Review.Test.error
-                            { message = "Let blocks can be joined together"
-                            , details = [ "Let blocks can contain multiple declarations, and there is no advantage to having multiple chained let expressions rather than one longer let expression." ]
-                            , under = "let"
-                            }
-                            |> Review.Test.atExactly { start = { row = 10, column = 5 }, end = { row = 10, column = 8 } }
-                            |> Review.Test.whenFixed """module A exposing (..)
-a =
-    let
-        b =
-            1
-
-        c =
-            1
-
-        d =
-            1
-
-        e =
-            1
-    in
-    b + c + d + e
-"""
-                        ]
-        ]
 
