3c539bac059ee534e4c817e02dcdadc915b480a0
lue-bird <lue.the.bird@gmail.com>
2023-10-11 14:37:00 +0200

    Improve error info of oneOfWeightedConstantsWithOneAndRestChecks

--- src/Simplify.elm
+++ src/Simplify.elm
@@ -9705,8 +9705,8 @@ oneOfWeightedConstantsWithOneAndRestChecks wrapper checkInfo =
             if listCollection.empty.is (extractInferResources checkInfo) otherOptionsArg then
                 Just
                     (Rule.errorWithFix
-                        { message = qualifiedToString checkInfo.fn ++ " with only one possible value can be replaced by " ++ qualifiedToString wrapper.wrap.fn
-                        , details = [ "Only a single value can be produced by this " ++ qualifiedToString checkInfo.fn ++ " call. You can replace the call with " ++ qualifiedToString wrapper.wrap.fn ++ " with the value." ]
+                        { message = qualifiedToString checkInfo.fn ++ " with one possible value will result in " ++ qualifiedToString wrapper.wrap.fn ++ " with that value"
+                        , details = [ "You can replace this call by " ++ qualifiedToString wrapper.wrap.fn ++ " with the first given value." ]
                         }
                         checkInfo.fnRange
                         (case AstHelpers.getTuple2 checkInfo.lookupTable checkInfo.firstArg of
