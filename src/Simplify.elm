24b6914042b3e4215e7f8139341277bc41c2b668
lue-bird <lue.the.bird@gmail.com>
2026-01-18 01:09:36 +0100

    mention that the filter to remove checks need expectNaN to be disabled

--- src/Simplify.elm
+++ src/Simplify.elm
@@ -1518,6 +1518,7 @@ Destructuring using case expressions
     Set.filter (\_ -> False) set
     --> Set.empty
 
+    -- when `expectNaN` is not enabled
     Set.filter (\k -> k /= specificKey) set
     --> Set.remove specificKey set
 
@@ -1639,6 +1640,7 @@ Destructuring using case expressions
     Dict.filter (\k _ -> f k) (Dict.map g dict)
     --> Dict.map g (Dict.filter (\k _ -> f k) dict)
 
+    -- when `expectNaN` is not enabled
     Dict.filter (\k _ -> k /= specificKey) dict
     --> Dict.remove specificKey dict
 
