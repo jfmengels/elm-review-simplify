3f74d0081b01e7d02265bca75e694b0741d72848
lue-bird <lue.the.bird@gmail.com>
2026-01-19 11:14:11 +0100

    infer calls while normalizing

--- tests/Simplify/DuplicatedIfsTest.elm
+++ tests/Simplify/DuplicatedIfsTest.elm
@@ -828,6 +828,32 @@ a =
   else 3
 """
                         ]
+        , test "should remove branches where the condition never matches (call)" <|
+            \() ->
+                """module A exposing (..)
+a =
+  if f <| x then
+    1
+  else if x |> f then
+    2
+  else
+    3
+"""
+                    |> Review.Test.run ruleWithDefaults
+                    |> Review.Test.expectErrors
+                        [ Review.Test.error
+                            { message = "The condition will always evaluate to False"
+                            , details = [ "The expression can be replaced by what is inside the 'else' branch." ]
+                            , under = "if"
+                            }
+                            |> Review.Test.atExactly { start = { row = 5, column = 8 }, end = { row = 5, column = 10 } }
+                            |> Review.Test.whenFixed """module A exposing (..)
+a =
+  if f <| x then
+    1
+  else 3
+"""
+                        ]
         , test "should remove branches where the condition always matches (literal on the left using /=)" <|
             \() ->
                 """module A exposing (..)
